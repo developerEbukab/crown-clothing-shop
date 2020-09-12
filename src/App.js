2797cd7f7f7f7cb1b5acd6e80fe5de096efe4f42
ChukwuEbuka Beluolisa <developerebukab@gmail.com>
2020-09-12 08:18:08 -0400

    moved directory and shop data to reducer

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Homepage}/>
           <Route exact path="/checkout" component={CheckoutPage}/>
-          <Route exact path='/shop' component={ShopPage}/>
+          <Route path='/shop' component={ShopPage}/>
           <Route 
             exact 
             path='/signin' 
